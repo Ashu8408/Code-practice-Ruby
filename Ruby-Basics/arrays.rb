0aee5858a6e895531b3d1f738cda55851d73e0af
ashuOO7 <ashjnavj@gmail.com>
2024-11-17 12:48:11 +0530

    more methods in array

--- Ruby-Basics/arrays.rb
+++ Ruby-Basics/arrays.rb
@@ -35,6 +35,20 @@ puts "#{a}"
 a.pop
 puts "#{a}"
 
+#------------------------------------------------------------------------------------------------------------------------
+#EACH_WITH_INDEX
+
+a.each_with_index { |val, indx| puts "The value at index #{indx} is #{val}"}
+
+#DELETE_AT
+
+puts "#{a}"
+a.delete_at(2)
+puts "#{a}"
+a.delete(90)
+puts "#{a}"
+
+
 
 #------------------------------------------------------------------------------------------------------------------------
 # 1. Creation and Initialization
@@ -82,7 +96,6 @@ p array2.fetch(-1)
 p array2.fetch(-10)
 p array2.fetch(-11)     #out of range error
 
-# at and fetch both are used to get elements, at reverts 'nil' whereas feetch reverts 'index error' at invalid indices
 
 p array2.first     # First element
 p array2.last      # Last element
@@ -90,8 +103,8 @@ p array2.values_at(0, 2, 4) # Multiple indexes
 
 
 #------------------------------------------------------------------------------------------------------------------------
-
 p array2.sample    # Random element
+
 
 #------------------------------------------------------------------------------------------------------------------------
 # 3. Modifying Elements
@@ -137,7 +150,53 @@ p array2
 
 
 #------------------------------------------------------------------------------------------------------------------------
-# 5. Transformations
+# 5. Iteration and Traversal
+
+
+puts "\n------------- Iteration and Traversal -------------"
+array2.each { |e| print "#{e} " }
+
+puts array2.each_with_index { |e, i| puts "Element #{e} at index #{i}" }
+a.each_with_index { |val, indx| puts "The value at index #{indx} is #{val}"}
+
+
+p array2.map { |e| e * 2 }     # access by value at e and do the operation
+array2.map! { |e| e + 1 }
+p array2
+
+
+
+#------------------------------------------------------------------------------------------------------------------------
+# 6. Searching and Filtering
+
+
+puts "\n------------- Searching and Filtering -------------"
+p array2.include?(3)                   # returns true or false
+p array2.include?(50)
+
+p array2.find { |e| e > 3 }            # returns just greater item
+p array2.find_all { |e| e > 3 }        # returns all greater items
+
+p array2.index(3)
+p array2.index(320)                 # nil if element doesn't exist
+
+p array2.select { |e| e > 3 }          #??
+p array2.reject { |e| e > 3 }          #??
+
+
+
+=begin  
+                                    Comparison Table
+Aspect	                    select	                                     reject
+Behavior	        Keeps elements that return true.	    Excludes elements that return true.
+Condition	        Elements satisfying the condition.	    Elements not satisfying the condition.
+Returned Array	    Includes matching elements.	Excludes    matching elements.
+Example Condition	            `{	                            e
+
+=end
+
+#------------------------------------------------------------------------------------------------------------------------
+# 7. Transformations
 
 
 puts "\n------------- Transformations -------------"
@@ -152,7 +211,7 @@ p array2.shuffle      # random shuffle
 
 
 #------------------------------------------------------------------------------------------------------------------------
-# 6. Set Operations
+# 8. Set Operations
 
 
 puts "\n------------- Set Operations -------------"
@@ -167,7 +226,7 @@ p ["a", "b", "c", "d"] - ["c", "d", "e", "f"]
 
 
 #------------------------------------------------------------------------------------------------------------------------
-# 7. Combining Arrays
+# 9. Combining Arrays
 
 
 puts "\n------------- Combining Arrays -------------"
@@ -175,6 +234,7 @@ p array2 + [8, 9]             # it creates a new array and adds the elements at 
 p array2.concat([10, 11])     # it modifies the same array to add the elements
 
 p array2.zip([1, 2, 3])
+p array2.zip(["a", 2, "c"])
 # The zip method in Ruby combines elements from two or more arrays into a single 
 # array of subarrays, where each subarray contains corresponding elements from the input arrays.
 
@@ -182,7 +242,7 @@ p [1, 2].product([3, 4])      # cross product of elemets in an array
 
 
 #------------------------------------------------------------------------------------------------------------------------
-# 8. Splitting and Partitioning
+# 10. Splitting and Partitioning
 
 
 puts "\n------------- Splitting and Partitioning -------------"
@@ -190,13 +250,34 @@ p array2.partition { |e| e.even? }      # creates two partition, one of even and
 
 
 #------------------------------------------------------------------------------------------------------------------------
-# 9. Conversion
+# 11. Conversion
 
 
 puts "\n------------- Conversion -------------"
 p array2.join(", ")             # join method converts the elements of an array into a single string with the given input as seperator
-p array2.to_h { |e| [e, e**2] } # Nested arrays only
 
 
+#------------------------------------------------------------------------------------------------------------------------
+# 12. Other Methods
 
 
+puts "\n------------- Other Useful Methods -------------"
+p array2.empty?
+p array2.length
+p array2.size
+
+
+#------------------------------------------------------------------------------------------------------------------------
+# Aliases (Map = Collect)
+
+
+puts "\n------------- Collect -------------"
+#   It iterates over each element in the collection,
+# applies the block of code provided, and returns a new array containing the results of the block's execution.
+
+p array2.collect { |e| e * 2 }
+
+words = ["hello", "world", "ruby"]
+p words.collect { |word| word.upcase }
+
+numbers = (1..5).collect { |n| n ** 2 }
